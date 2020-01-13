13b9a01543128be7f283858c199477b58f24373b
Lasse Haverinen <lasse.haverinen@oamk.fi>
2020-01-13 15:27:54 +0200

    Added demo of multiple mw per route

--- route-level-middleware.js
+++ route-level-middleware.js
@@ -15,7 +15,21 @@ function headerEchoer(req, res, next) {
     console.log(property + ' - ' + req.headers[property]);
   }
 
+  next();
+}
 
+function mw1(req, res, next) {
+  console.log('mw1 running');
+  next();
+}
+
+function mw2(req, res, next) {
+  console.log('mw2 running');
+  next();
+}
+
+function mw3(req, res, next) {
+  console.log('mw3 running');
   next();
 }
 
@@ -25,15 +39,20 @@ function headerEchoer(req, res, next) {
  * 
  *****************************/
 
-app.get('/', (req, res)  => {
-  console.log('Route handling started');
-  console.log('Middleware was activated at ' + req.mwActivationTime);
-  res.send('Hello, this route is working');
-  console.log('Route handling completed');
-});
-
+/*
+  Here a single middleware function called headerEchoer is given for a route.
+*/
 app.get('/demonstration', headerEchoer, (req, res) => {
   res.send('Another route which works');
 });
+
+/* 
+  Each route can be given multiple middleware by passing them as an array.
+*/
+app.get('/multiple', [mw1, mw2, mw3], (req, res)  => {
+  console.log('Route handling started');
+  res.send('Route with multiple middleware executed. See the server console log for mw output');
+});
+
 
 app.listen(PORT, () => console.log('Application started at http://localhost:' + PORT));
